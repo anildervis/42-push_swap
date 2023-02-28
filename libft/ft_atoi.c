9227d40be3a1ecf704ac65b2b99112d7ff59455f
anilalis <anildervisogluib@gmail.com>
2023-02-28 13:26:57 +0300

    atoi needs more error management

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -14,20 +14,26 @@ int	ft_atoi(const char *str)
 
 	number = 0;
 	sign = 1;
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
-		str++;
+	// getting arguments with tabs between them
 	if (*str == '-')
 		sign = -1;
 	if (*str == '+' || *str == '-')
 		str++;
-	while (ft_isdigit(*str))
+	// + veya - kullanımı var mı diye checker kontrolü
+	// eğer varsa -, +'dan sonraki ifadenin null olmamasına dikkat et 
+	while (*str != '\0')
 	{
-		number = (number * 10) + (*str - '0') * sign;
-		str++;
-		if (number > 2147483647)
-			return (-1);
-		if (number < -2147483648)
-			return (0);
+		if (ft_isdigit(*str))
+		{
+			number = (number * 10) + (*str - '0') * sign;
+			str++;
+			if (number > 2147483647)
+				exit(1); // error message on stderror
+			if (number < -2147483648)
+				exit(1); // is it alright to use negative numbers?
+		}
+		else
+			exit(1); // error message on stderror
 	}
 	return (number);
 }
